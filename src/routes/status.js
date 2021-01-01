19c86215456a67e62af44337450f0a5611a47094
Jogius <jogius.julius.makowski@gmail.com>
2021-01-01 14:23:02 +0100

    feat: status routes

--- src/routes/status.js
+++ src/routes/status.js
@@ -22,4 +22,34 @@ router.post('/stop', async (req, res) => {
   res.json()
 })
 
+router.post('/chat/start', async (req, res) => {
+  await knex('status').update({ chatting: true })
+  res.json()
+})
+
+router.post('/chat/stop', async (req, res) => {
+  await knex('status').update({ chatting: false })
+  res.json()
+})
+
+router.post('/vote/start', async (req, res) => {
+  await knex('status').update({ voting: true })
+  res.json()
+})
+
+router.post('/vote/stop', async (req, res) => {
+  await knex('status').update({ voting: false })
+  res.json()
+})
+
+router.post('/results/start', async (req, res) => {
+  await knex('status').update({ results: true })
+  res.json()
+})
+
+router.post('/results/stop', async (req, res) => {
+  await knex('status').update({ results: false })
+  res.json()
+})
+
 module.exports = router
